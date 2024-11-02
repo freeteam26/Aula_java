8c7afc291aaf45c2e5b201545afaaba52a4e57fe
Thiago Matos <thiagomat454@gmail.com>
2024-11-02 13:23:03 -0300

    adiciona uma nova variavel array

--- teste01.java
+++ teste01.java
@@ -5,6 +5,9 @@ public class teste01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int [] v = new int[n];  
+        
+
         int[] vet = new int[n];
         for (int i = 0; i < n; i++) {
             vet[i] = sc.nextInt();
